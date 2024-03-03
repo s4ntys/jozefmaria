27875af652d0909a44f3491699939fa257b32c71
SanTy <zubajsamuel@gmail.com>
2024-03-03 20:46:12 +0100

    1.0
    Spravené všetko

---
+++ nacitanie.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        document.getElementById('loading').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+    }, 3000); // 3 sekundy
+});
